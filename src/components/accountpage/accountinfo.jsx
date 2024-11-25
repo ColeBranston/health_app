e08082dbcca6b5ecf8d55777e883a7b93f46c07c
ColeBranston <118498961+ColeBranston@users.noreply.github.com>
2024-11-25 01:54:39 -0500

    added ternary operators to check if user info exists

--- src/components/accountpage/accountinfo.jsx
+++ src/components/accountpage/accountinfo.jsx
@@ -1,15 +1,16 @@
 import '../main.css'
 
-const accountinfo = () => {
+const accountinfo = ({user_name, user_email, user_password, user_clientid, user_clientsecret, auth_endpoint}) => {
     return (
         <div className='accountContainer'>
             <div className='accountInfo'>
-                <p className='bodyText'>Name: null</p>
-                <p className='bodyText'>Email: null</p>
-                <p className='bodyText'>Password: null</p>
+                <p className='bodyText'>Name: {user_name?user_name:"null"}</p>
+                <p className='bodyText'>Email: {user_email?user_email:"null"}</p>
+                <p className='bodyText'>Password: {user_password?user_password:"null"}</p>
                 <p className='bodyText'>API Details: </p>
-                <span className='tabbed'><p className='bodyText'>Client ID: null</p></span>
-                <span className='tabbed'><p className='bodyText'>Auth Endpoint: null </p></span>
+                <span className='tabbed'><p className='bodyText'>Client ID: {user_clientid?user_clientid:"null"}</p></span>
+                <span className='tabbed'><p className='bodyText'>Client Secret: {user_clientsecret?user_clientsecret:"null"}</p></span>
+                <span className='tabbed'><p className='bodyText'>Auth Endpoint: {auth_endpoint?auth_endpoint:"null"} </p></span>
             </div>
         </div>
     );
